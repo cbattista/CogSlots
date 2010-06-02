d8e929a49f33c91cb8685eba501deeeea7bd0b9c
christian <christian@christian.(none)>
2010-06-02 13:51:30 -0400

    CogSub.py contains the Subject class which is used to store and output and experimental subject's data.
    Methods are:
    
    -input data (trial, condition, value)
    -print data (human readable)
    -preserve (pickle self)

---
+++ CogSub.py
@@ -0,0 +1,58 @@
+import time
+import pickle
+
+class Subject:
+	def __init__(self, number="unknown", session="unknown", run="unknown"):		
+		self.number = number
+		self.session = session
+		self.run = run
+		self.date = time.localtime()
+		#create dictionary to hold trial results
+		self.fname = "%s_%s_%s_%s.csv" % (number, session, run, self.date)
+		self.results = {}
+
+	def inputData(self, trial, condition, value):
+		trial = str(trial)
+		print "ADDING %s, %s, %s" % (trial, condition, value)
+		if self.results.has_key(trial):
+			data = self.results[trial]
+			data[condition] = value
+			self.results[trial] = data
+		else:
+			data = {}
+			data[condition] = value
+			self.results[trial] = data					
+
+
+	def printData(self):	
+		trials = self.results.keys()
+		intTrials = []
+		for t in trials:
+			intTrials.append(int(t))
+		intTrials.sort()
+		trials = []
+		for t in intTrials:
+			trials.append(str(t))
+		f = open(self.fname, "w")
+		for t in trials:
+			line = t
+			trial = self.results[t]
+			trialKeys = trial.keys()
+			trialKeys.sort()
+			header = ""
+			for tk in trialKeys:
+				header = "%s,%s" % (header, tk)
+				line = "%s,%s" % (line, trial[tk])
+			header = header + "\n"			
+			line = line + "\n"
+			if t == "1":
+				f.write(header)
+			f.write(line)
+		f.close()
+
+	def preserve(self):
+		f = open("%s.cogsub" % self.number, "w")
+		pickle.dump(self, f)
+		f.close()
+
+
