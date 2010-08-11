e5920f6953f4071f62d86403c934cacc6f35d000
cbattista <battista.christian@gmail.com>
2010-08-11 09:44:27 -0400

    addition of the 'any' symbol

--- cfg.py
+++ cfg.py
@@ -8,7 +8,8 @@ IM_CHERRIES = "images/cherries.png"
 IM_CLOVER = "images/clover.png"
 IM_GOLDBARS = "images/gold_bullion.png"
 IM_TREASURECHEST = "images/chest.png"
-IM_EMPTY = "images/spacer.gif"
+IM_BLANK = "images/spacer.gif"
+IM_EMPTY = "images/any.png"
 
 # pack them in to a tuple to make things easier
 symbols = [IM_BAR, IM_BELL, IM_CHERRIES, IM_CLOVER, IM_GOLDBARS, IM_TREASURECHEST, IM_EMPTY]
